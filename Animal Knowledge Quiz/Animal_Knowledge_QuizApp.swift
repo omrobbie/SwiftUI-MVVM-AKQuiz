63028bf5e742f1267c23f75afd72115bdc617282
omrobbie <putu.roby@yahoo.com>
2022-03-03 13:07:15 +0800

    Initial Commit

---
+++ Animal Knowledge Quiz/Animal_Knowledge_QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animal_Knowledge_QuizApp.swift
+//  Animal Knowledge Quiz
+//
+//  Created by omrobbie on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Animal_Knowledge_QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animal Knowledge Quiz/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animal Knowledge Quiz
+//
+//  Created by omrobbie on 03/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
