bef7f03fe908cd7da67a74c17da381f776b5f4d1
omrobbie <putu.roby@yahoo.com>
2022-03-03 13:15:21 +0800

    add and adjust background screen

--- Animal Knowledge Quiz/ContentView.swift
+++ Animal Knowledge Quiz/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .ignoresSafeArea()
+            LinearGradient(colors: [.purple.opacity(0.4), .blue.opacity(0.4)], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+        }
     }
 }
 
